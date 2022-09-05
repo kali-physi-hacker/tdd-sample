1741ac6d99ada559b691b17028926a25ab07965f
Kali Junior Brown <browndesmond30@yahoo.com>
2022-09-05 09:49:32 +0000

    Added a couple test cases

---
+++ tests.py
@@ -0,0 +1,17 @@
+def test_case_one():
+    """
+    Tests case does nothing just passing
+    :returns:
+    """
+    result = 6
+    assert result == 6
+
+
+def test_case_two():
+    """
+    Tests case does nothing just fails
+    :returns:
+    """
+    result = 9
+    assert result == 8
+
